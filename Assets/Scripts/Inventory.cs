70a827abc5ed0a861343b2ac7458b7e6778ec700
Morgan 6 Freedom <morgan.klein@6freedom.studio>
2021-02-24 19:07:17 +0100

    second commit

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using DG.Tweening;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private GameObject socketPrefab;
+
+    private List<GameObject> objectList = new List<GameObject>();
+    private List<GameObject> sockets = new List<GameObject>();
+    private List<Vector3> socketsPos = new List<Vector3>();
+
+
+    private void Start()
+    {
+        GetAllPointsOnCircle(transform.position, 12, 0.07f);
+    }
+
+    private void AddObject(GameObject _newObject)
+    {
+        objectList.Add(_newObject);
+    }
+
+    private void RemoveObject(GameObject _removedObject)
+    {
+        objectList.Add(_removedObject);
+    }
+
+    /// <summary>
+    /// Will generate multiple Socket in circle around the wrist inventory
+    /// </summary>
+    /// <param name="center">The center of the inventory</param>
+    /// <param name="maxSockets">The number of Socket you want</param>
+    /// <param name="radius">How far away from the center are the sockets ?</param>
+    private void GetAllPointsOnCircle(Vector3 center, int maxSockets, float radius)
+    {
+        float step = 2 * Mathf.PI / maxSockets;
+        for (int i = 0; i < maxSockets; i++)
+        {
+            float x = Mathf.Cos(i * step);
+            float y = Mathf.Sin(i * step);
+            Vector3 pos = center + new Vector3(x, y, 0) * radius;
+            GameObject socket = Instantiate(socketPrefab, transform.position, Quaternion.identity, transform);
+            socket.SetActive(false);
+            sockets.Add(socket);
+            socketsPos.Add(pos);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Hand"))
+            return;
+
+        for (int i = 0; i < sockets.Count; i++)
+        {
+            GameObject s = sockets[i];
+            s.transform.DOScale(0, 0.5f);
+            s.transform.DOLocalMove(transform.position, 0.5f).OnComplete(() =>
+            {
+                s.SetActive(false);
+            });
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Hand"))
+            return;
+
+        for (int i = 0; i < sockets.Count; i++)
+        {
+            sockets[i].SetActive(true);
+            sockets[i].transform.DOScale(1.0f, 0.5f);
+            sockets[i].transform.DOLocalMove(socketsPos[i], 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/WristSocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class WristSocket : XRSocketInteractor
+{
+
+    // Is the socket active, and object is being held by different interactor
+    public override bool isSelectActive => base.isSelectActive;
+
+    public override XRBaseInteractable.MovementType? selectedInteractableMovementTypeOverride
+    {
+        // Move while ignoring physics, and no smoothing
+        get { return XRBaseInteractable.MovementType.Instantaneous; }
+    }
+
+    protected override void OnHoverEntering(XRBaseInteractable interactable)
+    {
+
+    }
+    protected override void OnHoverExiting(XRBaseInteractable interactable)
+    {
+
+    }
+
+    protected override void OnSelectEntering(XRBaseInteractable interactable)
+    {
+
+    }
+
+    protected override void OnSelectExiting(XRBaseInteractable interactable)
+    {
+
+    }
+}
